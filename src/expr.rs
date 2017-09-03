5b58b870726dba098f0b19a0372f2ac20e4e6327
Peng Guanwen <pgw1999@outlook.com>
2017-09-03 12:18:58 +0800

    Add Expr enum

---
+++ src/expr.rs
@@ -0,0 +1,31 @@
+use std::sync::Mutex;
+use num::BigInt;
+use symtern::prelude::*;
+use symtern::{Pool, Sym};
+use symtern::adaptors::{Inline, self};
+
+#[derive(Debug)]
+pub enum Expr {
+    Integer(BigInt),
+    Symbol(Symbol),
+    Float(f64),
+
+    Product(Box<Expr>, Box<Expr>),
+}
+
+type InlinePool = Inline<Pool<str, u32>>;
+type InlineSym = adaptors::InlineSym<Sym<u32>>;
+
+lazy_static! {
+    static ref sympool: Mutex<InlinePool> = Mutex::new(Inline::from(Pool::<str, u32>::new()));
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Symbol(InlineSym);
+
+impl Symbol {
+    fn new(s: &str) -> Symbol {
+        let sym = sympool.lock().unwrap().intern(s).unwrap();
+        Symbol(sym)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+extern crate num;
+extern crate symtern;
+#[macro_use] extern crate lazy_static;
+
+mod expr;
+
+pub use expr::Expr as Expr;
